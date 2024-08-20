d35252155625949751d6cd5037e29a101414abcd
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-08-20 13:48:52 +0900

    Update chunk_libriheavy.py

--- utility_scripts/chunk_libriheavy_manifests/chunk_libriheavy.py
+++ utility_scripts/chunk_libriheavy_manifests/chunk_libriheavy.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 def split_jsonl_gz_file(input_file, lines_per_chunk=10000):
     # Extract directory and base filename
-    output_dir = os.path.dirname(input_file) + os.path.split(input_file)[1].split('.')[0].split('_')[-1]
+    output_dir = os.path.dirname(input_file) + os.sep + os.path.split(input_file)[1].split('.')[0].split('_')[-1]
     base_name = os.path.basename(input_file).rsplit('.', 2)[0]  # Remove .jsonl.gz
 
     print(output_dir, base_name)
