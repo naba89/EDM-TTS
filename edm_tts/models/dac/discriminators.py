21fd6e9a9878e07e883e7b934b481f6f5f938b00
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-08-20 14:03:16 +0900

    Update discriminators.py

--- edm_tts/models/dac/discriminators.py
+++ edm_tts/models/dac/discriminators.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchaudio
 from einops import rearrange
-from torch.nn.utils import weight_norm
+from torch.nn.utils.parametrizations import weight_norm
 from transformers import PreTrainedModel, PretrainedConfig, AutoConfig, AutoModel
 
 
